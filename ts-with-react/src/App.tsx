f4657361811fdc64ad2a3ce9dd0bd8fbdf98c469
sixiyxy <771702500@qq.com>
2021-09-07 13:58:49 +0800

    3

--- ts-with-react/src/App.tsx
+++ ts-with-react/src/App.tsx
@@ -1,24 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 import Hello from './components/Hello';
 import LikeButton from './components/LikeButton';
 import MouseTracker from './components/MouseTracker';
 import useMousePosition from './hooks/useMousePosition';
+import useURLLoader from './hooks/useURLLoader';
 
 import { useState } from 'react';
 
+interface IShowResult {
+  message: string;
+  status: string;
+}
+
+interface IThemeProps {
+  [key: string]: {color: string; background: string;}
+}
+const themes: IThemeProps = {
+  'light': {
+    color: '#000',
+    background: '#eee',
+  },
+  'dark': {
+    color: '#fff',
+    background: '#222',
+  }
+}
+
+export const ThemeContext = React.createContext(themes.dark)
 
 function App() {
   const [ show, setShow ] = useState(true)
+  const [ data, loading ] = useURLLoader('https://dog.ceo/api/breeds/image/random',[show])
+  const dogResult = data as IShowResult
+   
   const positions = useMousePosition()
   return (
     <div className="App">
+      <ThemeContext.Provider value={themes.dark}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <button onClick={() => {setShow(!show)}}>Toggle Tracker </button>
+          <button onClick={() => {setShow(!show)}}>Refresh dog photo </button>
         </p>
+        { loading? <p>🐕 读取中</p>
+          :<img src={dogResult && dogResult.message} />}
         <p>X: {positions.x}, Y : {positions.y}</p>
         <LikeButton />
         <Hello />
@@ -32,6 +59,7 @@ function App() {
           Learn React
         </a>
       </header>
+      </ThemeContext.Provider>
     </div>
   );
 }
--- ts-with-react/src/components/LikeButton.tsx
+++ ts-with-react/src/components/LikeButton.tsx
@@ -1,18 +1,51 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef, useContext } from 'react'
 import useMousePosition from '../hooks/useMousePosition'
+import { ThemeContext } from '../App'
 
 const LikeButton: React.FC = () => {
    const [like, setLike] = useState(0)
    const [on, setOn] = useState(true)
+   const likeRef = useRef(0)
+   const didMountRef = useRef(false)
+   const domRef = useRef<HTMLInputElement>(null)
+   const theme = useContext(ThemeContext)
    const positions = useMousePosition()
+   const style = {
+       background: theme.background,
+       color: theme.color,
+   }
    useEffect(() => {
        console.log('document title effect is running')
        document.title = `点击了${like}次`
    },[like])
+
+   useEffect(() => {
+        if (didMountRef.current){
+            console.log('this is updated')
+        } else{
+            didMountRef.current = true
+        }
+
+   })
+
+   useEffect(() => {
+        if (domRef && domRef.current){
+            domRef.current.focus()
+        }
+   })
+
+   function handleAlertClick() {
+       setTimeout(() => {
+           alert('you clicked on ' + likeRef.current)
+       },3000)
+    }
+
+
    return (
         <>
+        <input type="text" ref={domRef} />
         <p>X: {positions.x}, Y : {positions.y}</p>
-        <button onClick={() => {setLike(like + 1)}}>
+        <button style={style} onClick={() => {setLike(like + 1); likeRef.current++}}>
            {like} 👍
         </button>
         <button onClick={() => {setOn(!on)}}>
