6be437ec3799c3aecb109429b242be60279ef818
sixiyxy <771702500@qq.com>
2021-09-14 16:55:34 +0800

    4-8

--- vikingship/src/App.tsx
+++ vikingship/src/App.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
-
+import Button, { ButtonType, ButtonSize } from './components/Button/button';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
-        <h2>Hello World</h2>
-        <h3>Hello World</h3>
-        <hr/>
-        <code>
-          const a = 'b'
-        </code>
-        <p>
+        <Button>Hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Hello </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com"> Baidu Link </Button>
+        <p> 
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
