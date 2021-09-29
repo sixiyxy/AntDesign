e0112f8424bd4a572146819b869b403d16129c22
sixiyxy <771702500@qq.com>
2021-09-29 16:49:28 +0800

    6-3

--- vikingship/src/App.tsx
+++ vikingship/src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Button, { ButtonType, ButtonSize } from './components/Button/button';
-
+import Menu from './components/Menu/menu';
+import MenuItem from './components/Menu/menuItem';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Menu defaultIndex={0} onSelect={(index) => {alert(index)}}>
+          <MenuItem index={0}>
+            cool link
+          </MenuItem>
+          <MenuItem index={1}>
+            cool link2
+          </MenuItem>
+          <MenuItem index={2}>
+            cool link3
+          </MenuItem>
+      </Menu>
         <Button autoFocus>Hello</Button>
         <Button btnType={ButtonType.Default} disabled> Disabled Button </Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
---
+++ vikingship/src/components/Menu/menuItem.tsx
@@ -0,0 +1,32 @@
+import React,{ useContext } from "react";
+import classNames from "classnames";
+import { MenuContext } from "./menu";
+
+export interface MenuItemProps {
+    index: number;
+    disabled?: boolean;
+    className?: string;
+    style?: React.CSSProperties;
+}
+
+const MenuItem: React.FC<MenuItemProps> = (props) => {
+    // eslint-disable-next-line
+    const { index, disabled, className, style, children } = props;
+    const context = useContext(MenuContext)
+    const classes = classNames('menu-item', className, {
+        'isdisabled': disabled,
+        'is-activate': context.index === index
+    })
+    const handleClick = () => {
+        if(context.onSelect && !disabled){
+            context.onSelect(index)
+        }
+    }
+    return (
+        <li className={classes} style={style} onClick={handleClick}>
+            {children}
+        </li>
+    )
+}
+
+export default MenuItem;
