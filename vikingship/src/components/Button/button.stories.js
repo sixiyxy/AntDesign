7c8fc4658cc290faa784ff54c736e54541fe60cc
sixiyxy <771702500@qq.com>
2021-10-12 21:43:40 +0800

    8-4

--- vikingship/src/components/Button/button.stories.js
+++ vikingship/src/components/Button/button.stories.js
@@ -3,10 +3,26 @@ import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 
 import Button from './button'
- 
+
 const defaultButton = () => (
     <Button onClick={action('clicked')}> default button </Button>
 )
 
+const buttonWithSize = () => (
+    <>
+        <Button size="lg">Large button</Button>
+        <Button size="sm">Small button</Button>
+    </>
+)
+
+const buttonWithType = () => (
+    <>
+        <Button btnType="primary">primary button</Button>
+        <Button btnType="danger">danger button</Button>
+        <Button btnType="link" href="https://baidu.com">link button</Button>
+    </>
+)
 storiesOf('Button Component', module)
     .add('默认 Button', defaultButton)
+    .add('不同尺寸的Button', buttonWithSize)
+    .add('不同类型的Button',buttonWithType)
