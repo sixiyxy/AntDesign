46c274b9fb039b2c9614612ae8b852d436a53bfa
sixiyxy <771702500@qq.com>
2021-10-27 17:24:08 +0800

    9-6

---
+++ vikingship/src/components/AutoComplete/autoComplete.stories.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { AutoComplete } from "./autoComplete";
+
+const SimpleComplete = () => {
+    const lakers = ['bradley', 'pope', 'caruso', 'cook', 'cousins','james', 'AD', 'green', 'howard', 'kuzma', 'McGee', 'rando']
+    const lakersWithNumber = [
+          {value: 'bradley', number: 11},
+          {value: 'pope', number: 1},
+          {value: 'caruso', number: 4},
+          {value: 'cook', number: 2},
+          {value: 'cousins', number: 15},
+          {value: 'james', number: 23},
+          {value: 'AD', number: 3},
+          {value: 'green', number: 14},
+          {value: 'howard', number: 39},
+          {value: 'kuzma', number: 0},
+    ]
+    
+    // const handleFetch = (query: string) => {
+    //     return lakers.filter(name => name.includes(query))
+    // }
+    const handleFetch = (query: string) => {
+        
+    }
+    const renderOption = (item: string) => {
+        return (
+            <h2>Name: {item}</h2>
+        )
+    }
+    return (
+        <AutoComplete
+            fetchSuggestions={handleFetch}
+            onSelect={action('selected')}
+            renderOption = {renderOption}
+        />
+    )
+}
+
+storiesOf('AutoComplete Component', module)
+        .add('AutoComplete', SimpleComplete)
