fcce1ded2786cb76745c2c42704e85589a0da20e
sixiyxy <771702500@qq.com>
2021-11-01 16:35:29 +0800

    10-6

--- vikingship/src/components/Upload/upload.tsx
+++ vikingship/src/components/Upload/upload.tsx
@@ -6,9 +6,11 @@ import Button from '../Button/button'
 
 interface UploadProps {
     action: string;
+    beforeUpload? : (file: File) => boolean | Promise<File>
     onProgress?: (percentage: number, file: File) => void;
     onSuccess?: (data: any, file: File) => void;
     onError?: (err: any, file: File) => void;
+    onChange?: (file: File) => void;
 }
 
 export const Upload: FC<UploadProps> = (props) => {
@@ -17,6 +19,8 @@ export const Upload: FC<UploadProps> = (props) => {
         onProgress,
         onSuccess,
         onError,
+        beforeUpload,
+        onChange,
     } = props
     const fileInput = useRef<HTMLInputElement>(null)
     const handleClick = () => {
@@ -34,10 +38,8 @@ export const Upload: FC<UploadProps> = (props) => {
             fileInput.current.value = ""
         }
     }
-    const uploadFiles = (files: FileList) => {
-         let postFiles = Array.from(files)
-         postFiles.forEach(file => {
-             const formData = new FormData()
+    const post = (file: File) => {
+        const formData = new FormData()
              formData.append(file.name, file)
              axios.post(action, formData, {
                  headers: {
@@ -56,12 +58,35 @@ export const Upload: FC<UploadProps> = (props) => {
                  if(onSuccess){
                      onSuccess(resp.data, file)
                  }
+                 if(onChange) {
+                     onChange(file);
+                 }
              }).catch(err => {
                  console.error(err)
                  if(onError){
                      onError(err, file)
                  }
+                 if(onChange) {
+                    onChange(file);
+                }
              })
+    }
+    const uploadFiles = (files: FileList) => {
+         let postFiles = Array.from(files)
+         postFiles.forEach(file => {
+             if(!beforeUpload){
+                post(file);
+             }else {
+                 const result = beforeUpload(file)
+                 if (result && result instanceof Promise){
+                     result.then(processedFile => {
+                        post(processedFile)
+                     })
+                 } else if (result !== false){
+                    post(file)
+                 }
+             }
+             
          }
 
          )
