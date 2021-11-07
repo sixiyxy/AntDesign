aa88956a9ba15762e8ea4f5533e1f8c2d64b5486
sixiyxy <771702500@qq.com>
2021-11-07 15:14:07 +0800

    12-5

--- vikingship/.storybook/preview.tsx
+++ vikingship/.storybook/preview.tsx
@@ -10,7 +10,7 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 library.add(fas)
-import { addDecorator } from '@storybook/react';
+import { addDecorator, configure } from '@storybook/react';
 import React from 'react';
 import '../src/styles/index.scss'
 import { withInfo } from '@storybook/addon-info'
@@ -35,3 +35,14 @@ const storyWrapper = (storyFn: any) => (
 addDecorator(storyWrapper)
 addDecorator(withInfo)
 addParameters({info: { inline: true, header: false}})
+
+const loaderFn = () => {
+    const allExports = [require('../src/welcome.stories.tsx')];
+    const req = require.context('../src/components', true, /\.stories\.tsx$/);
+    req.keys().forEach(fname => allExports.push(req(fname)));
+    return allExports;
+};
+
+
+// automatically import all files ending in *.stories.js
+configure(loaderFn, module);
